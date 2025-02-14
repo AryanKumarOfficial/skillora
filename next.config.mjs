23f524eeee3c3cb8afb617f28ee51194fc532a9a
Aryan Kumar <aryanak9163@gmail.com>
2025-02-15 01:49:25 +0530

    fixes the problem with base url

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         unoptimized: true
     },
     trailingSlash: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "/",
+    basePath: "/next-school-web-app"
 };
 
 export default nextConfig;
