76e4c3359a90ec1831c7e0ed8c4b57a9779a3a74
Aryan Kumar <aryanak9163@gmail.com>
2025-02-15 18:35:34 +0530

    adds mentors section

--- src/app/(group1)/about-us/Teachers.jsx
+++ src/app/(group1)/about-us/Teachers.jsx
@@ -3,6 +3,8 @@ import SectionName from '@/components/ui/sectionName'
 import Title from '@/components/ui/title'
 import {teamData} from '@/lib/fackdata/teamData'
 import TeacherCard from './TeacherCard'
+import {Button} from '@/components/ui/button'
+import Link from 'next/link'
 
 const Teachers = () => {
     return (
@@ -14,10 +16,17 @@ const Teachers = () => {
                 </div>
                 <div className="lg:pt-15 pt-10">
                     <div className="grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-7.5">
-                        {teamData.map(({id, name, position, src}) => <TeacherCard key={id} name={name}
-                                                                                  position={position}
-                                                                                  src={src}/>)}
+                        {teamData.slice(0, 8).map(({id, name, position, src}) => (
+                            <TeacherCard key={id} name={name} position={position} src={src}/>
+                        ))}
                     </div>
+                </div>
+                <div className="flex justify-center mt-10">
+                    <Button variant="pill"
+                            className="bg-primary text-cream-foreground border-primary hover:bg-opacity-90 transition-all duration-300 shadow-lg"
+                            asChild>
+                        <Link href="/teachers">See More</Link>
+                    </Button>
                 </div>
             </div>
         </section>
